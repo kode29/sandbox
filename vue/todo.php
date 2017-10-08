27f0492e1a8e04f9efd7f9497382fcb3920b9815
Kyle M. Perkins <kp@q5i.co>
2017-10-09 01:28:38 +0900

    Successfully added function to enter textbox to WebSQL

--- vue/todo.php
+++ vue/todo.php
@@ -9,7 +9,7 @@ https://vuejs.org/v2/guide/#Handling-User-Input
 https://medium.freecodecamp.org/vue-js-introduction-for-people-who-know-just-enough-jquery-to-get-by-eab5aa193d77
 
 TODO:
-Add localStorage system for keeping todo items and retrieving 
+Add localStorage system for keeping todo items and retrieving
 -->
 <!doctype html>
 <html lang="en">
@@ -20,7 +20,7 @@ Add localStorage system for keeping todo items and retrieving
 
   </head>
 
-  <body>
+  <body onload="init()">
     <div id="todo-list">
       <ol>
         <todo-item
@@ -37,6 +37,64 @@ Add localStorage system for keeping todo items and retrieving
 
 
     <script>
+    //HTML5 Local Storage
+    function init() {
+      vueDB.webdb.open();
+      vueDB.webdb.createTable();
+    }
+
+    //Prereq setup
+    var vueDB = {};
+    vueDB.webdb = {};
+
+    //Initial setup
+    vueDB.webdb.open = function(){
+      var dbSize = 2 * 1024 * 1024; //2MB
+      // DB Setup: name, Version, Description, Size
+      vueDB.webdb.db = openDatabase("Todo", "1", "Todo Manager", dbSize);
+    }
+
+    // Checking
+    vueDB.webdb.onError = function(tx, e){
+      alert("There was an error setting up the local database: " + e.message);
+    }
+
+    // Success
+    vueDB.webdb.onSuccess = function(tx, r){
+      //re-render the data
+      //vueDB.webdb.getAllTodoItems(loadTodoItems);
+    }
+
+    vueDB.webdb.createTable = function() {
+      var db = vueDB.webdb.db;
+      db.transaction(function(tx) {
+      tx.executeSql("CREATE TABLE IF NOT EXISTS " +
+                "todo(ID INTEGER PRIMARY KEY ASC, todo TEXT, added_on DATETIME)", []);
+      });
+    }
+
+//    vueDB.webdb.getAllTodoItems = function(renderFunc) {
+//  var db = vueDB.webdb.db;
+//  db.transaction(function(tx) {
+//    tx.executeSql("SELECT * FROM todo", [], renderFunc,
+//        vueDB.webdb.onError);
+//  });
+//}
+
+vueDB.webdb.addTodo = function(todoText) {
+  var db = vueDB.webdb.db;
+  db.transaction(function(tx){
+    var addedOn = new Date();
+    tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)",
+      [todoText, addedOn],
+      vueDB.webdb.onSuccess,
+      vueDB.webdb.onError);
+  });
+}
+
+
+    // Begin Vue.js
+
     Vue.component('todo-item', {
       props: ['todo'],
       template: '<li>{{ todo.text }}</li>'
@@ -59,12 +117,15 @@ Add localStorage system for keeping todo items and retrieving
       methods: {
         addItem: function(){
           // Basic function setup
+          var i = this.newItem
 
           // Push to the array
-          tdList.todos.push({ text: this.newItem })
+          tdList.todos.push({ text: i })
 
           // Reset the field (attaches to "data")
           this.newItem = "";
+
+          vueDB.webdb.addTodo(i)
         }
       }
     })
